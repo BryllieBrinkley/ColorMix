f2d92308e11a0f8f94c93024fd90d33595d6ab2f
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-23 10:30:39 -0400

    Add functionality to switch the background color of  using a UISwitch and IBAction

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView!
+
     
     override func viewDidLoad() {
         colorView.backgroundColor = .black
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            colorView.backgroundColor = .red
+        } else {
+            colorView.backgroundColor = .black
+        }
+    }
+    
 
 }
 
