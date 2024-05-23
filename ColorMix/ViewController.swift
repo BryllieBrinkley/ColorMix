8b58cea9a966ba0ca249bc9fab7b6d1cecc57769
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-23 10:17:22 -0400

    started the incremental development process by putting a view onscreen to display the color

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Jibryll Brinkley on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        colorView.backgroundColor = .black
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
